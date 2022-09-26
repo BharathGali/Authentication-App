ca3b11bc13b54e23f02c2bc6be268c18717ba8e9
Nethra1 <vijayadasnethra97@gmail.com>
2022-09-26 13:28:52 -0400

    Login and signup

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiServiceService } from '../api-service.service';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+  uname: string = "";
+  pass: string = "";
+  email: string = "";
+  constructor(private route: Router,private service: ApiServiceService) { }
+
+  ngOnInit(): void {
+    console.log("sign");
+  }
+
+  goToLogin(): void{
+    this.route.navigate(['/login'])
+  }
+
+  signUpUser(): void{
+      var formData: any = new FormData();
+      formData.append("uname", this.uname);
+      formData.append("pass", this.pass);
+      formData.append("email", this.email);
+      this.service.registerUser(formData).subscribe(response =>{
+        if(response == "OK"){
+          console.log("success")
+        }else{
+          console.log("error");
+        }
+      });
+  }
+
+}
